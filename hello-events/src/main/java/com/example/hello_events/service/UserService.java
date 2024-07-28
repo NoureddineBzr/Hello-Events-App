c3ff82aa29d04ebc64d5d0e2a03e4210831f65a9
Noureddine Bouzire <127704618+NoureddineBzr@users.noreply.github.com>
2024-07-28 16:23:28 +0100

    Delete hello-events/src directory

--- hello-events/src/main/java/com/example/hello_events/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.hello_events.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-    UserDetailsService userDetailsService();
-}
